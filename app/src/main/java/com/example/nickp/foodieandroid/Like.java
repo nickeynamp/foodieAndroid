2a511c6366952da943c175b6b58953c33539a665
Nick <namp@kth.se>
2017-02-13 14:47:05 +0100

    Adding pages for navigation

---
+++ app/src/main/java/com/example/nickp/foodieandroid/Like.java
@@ -0,0 +1,13 @@
+package com.example.nickp.foodieandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Like extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_like);
+    }
+}

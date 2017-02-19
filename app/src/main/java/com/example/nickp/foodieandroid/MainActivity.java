34cfed581961d4fea06af3b59c9debd3edf7cf88
Nick <namp@kth.se>
2017-02-19 01:57:12 +0100

    adding drawer, in process

--- app/src/main/java/com/example/nickp/foodieandroid/MainActivity.java
+++ app/src/main/java/com/example/nickp/foodieandroid/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.nickp.foodieandroid;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends ActionBarHandler {
+
+    private DrawerLayout jDrawer;
+    private ListView jList;
+    private String[] items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState, R.layout.activity_main);
+        jDrawer = (DrawerLayout) findViewById(R.id.drawer);
+        jList = (ListView) findViewById(R.id.navItems);
+
+        //set shadow to mask main content when drawer opens if there exist PNGs to mask
+        //jDrawer.setDrawerShadow(R.drawable.dr);
+
+        //set the adapter for the list view
+        jList.setAdapter(new ArrayAdapter<String>(this,R.layout.drawer_list_item,items));
+
+        //set the list click's listener
+        jList.setOnItemClickListener(new DrawerItemClickListener());
     }
 
     public void goToBrowse(View view) {

d4f10d78eb99b1474ccbab6dd166f5a66f20df3b
philippe <philippe.tillheden@icloud.com>
2017-02-19 17:40:06 +0100

    Fix browse and like page

--- app/src/main/java/com/example/nickp/foodieandroid/MainActivity.java
+++ app/src/main/java/com/example/nickp/foodieandroid/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends ActionBarHandler {
@@ -17,16 +16,16 @@ public class MainActivity extends ActionBarHandler {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState, R.layout.activity_main);
         jDrawer = (DrawerLayout) findViewById(R.id.drawer);
-        jList = (ListView) findViewById(R.id.navItems);
+        //jList = (ListView) findViewById(R.id.navItems);
 
         //set shadow to mask main content when drawer opens if there exist PNGs to mask
         //jDrawer.setDrawerShadow(R.drawable.dr);
 
         //set the adapter for the list view
-        jList.setAdapter(new ArrayAdapter<String>(this,R.layout.drawer_list_item,items));
+        //jList.setAdapter(new ArrayAdapter<String>(this,R.layout.drawer_list_item, items));
 
         //set the list click's listener
-        jList.setOnItemClickListener(new DrawerItemClickListener());
+        //jList.setOnItemClickListener(new DrawerItemClickListener());
     }
 
     public void goToBrowse(View view) {
